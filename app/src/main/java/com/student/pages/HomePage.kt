d78f40c11695db2d2488155fd1108ceaec8a6158
aluno <etecae023@hotmail.com>
2025-08-13 12:04:40 -0300

    feat: adiciona front de login e signup

--- app/src/main/java/com/student/pages/HomePage.kt
+++ app/src/main/java/com/student/pages/HomePage.kt
@@ -1,10 +1,9 @@
-package com.student.views
+package com.student.pages
 
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -14,9 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -26,29 +23,25 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
+import androidx.navigation.NavController
 
 @Composable
-fun Home() {
+fun HomePage(modifier: Modifier = Modifier, navController: NavController) {
     var name by remember { mutableStateOf("") }
     var names = remember { mutableStateListOf<String>() }
     var presents = remember { mutableStateListOf<String>() }
@@ -208,17 +201,5 @@ fun Home() {
                 }
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun HomePreview() {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
-        Home()
     }
 }
---
+++ app/src/main/java/com/student/pages/LoginPage.kt
@@ -0,0 +1,129 @@
+package com.student.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun LoginPage(modifier: Modifier = Modifier, navController: NavController) {
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Página de login", fontSize = 32.sp, color = Color.White)
+
+        Spacer(modifier = Modifier.height(28.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = "Email:", color = Color.White.copy(0.5f), fontSize = 12.sp)
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.9f), shape = RoundedCornerShape(8.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFF181818),
+                unfocusedBorderColor = Color.White.copy(0.1F),
+                focusedContainerColor = Color(0xFF181818),
+                focusedBorderColor = Color.White.copy(0.1f)
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "Senha:", color = Color.White.copy(0.5f), fontSize = 12.sp)
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.9f), shape = RoundedCornerShape(8.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFF181818),
+                unfocusedBorderColor = Color.White.copy(0.1F),
+                focusedContainerColor = Color(0xFF181818),
+                focusedBorderColor = Color.White.copy(0.1f)
+            )
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("home")
+            },
+            modifier = Modifier
+                .height(39.dp)
+                .width(260.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFB29146),
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = "Entrar", fontSize = 13.sp)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextButton(onClick = {
+            navController.navigate("signup")
+        }) {
+            Text(
+                buildAnnotatedString {
+                    append("Não tem uma conta? ")
+
+                    withStyle(style = SpanStyle(color = Color(0XFFB29146))) {
+                        append("Cadastre-se")
+                    }
+                },
+                color = Color.White, fontSize = 13.sp
+            )
+        }
+    }
+}
